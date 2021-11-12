1b4e20dbfa75bc7bdb8e714a9a6a8ab4621bce50
Raquel <raquelshc@outlook.com>
2021-11-12 23:26:13 +0100

    Ejercicio 7

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ejercicios_jdbc_2_1-raquel-alberto
+
+### ***Ejercicio 7***
+
+Con este método lo que hacemos es conseguir los nombres de los campos 
+de la base de datos librería. El resultado que obtenemos es el siguiente:
+
+[ISBN, TITULO, AUTOR, EDITORIAL, PAGINAS, COPIAS]
--- src/main/java/org/iesinfantaelena/dao/LibrosDAO.java
+++ src/main/java/org/iesinfantaelena/dao/LibrosDAO.java
@@ -3,9 +3,14 @@ package org.iesinfantaelena.dao;
 import org.iesinfantaelena.modelo.AccesoDatosException;
 import org.iesinfantaelena.modelo.Libro;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface LibrosDAO {
+
+    public void cerrar() throws AccesoDatosException;
+
+    public void liberar() throws AccesoDatosException;
 
     public List<Libro> verCatalogo() throws AccesoDatosException;
 
@@ -15,8 +20,11 @@ public interface LibrosDAO {
 
     public void borrar(Libro libro) throws AccesoDatosException;
 
-    public void obtenerLibro(int ISBN) throws AccesoDatosException;
-
     public String[] getCamposLibro() throws AccesoDatosException;
 
+    public void obtenerLibro(int ISBN) throws AccesoDatosException;
+
+    public boolean crearTablaLibros() throws SQLException;
+
+    public void librosporEditorial(String editorial) throws AccesoDatosException;
 }
